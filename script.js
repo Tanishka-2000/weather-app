c5c0a2f15ad7342ae72b8f094642ddede01aaa0e
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-07-01 11:08:00 -0700

    add giphy and change background color

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
 const input = document.querySelector('input');
-const output = document.querySelector('div');
+const main = document.querySelector('.main');
+const output = document.querySelectorAll('.weather-data div');
+const img = document.querySelector('.gif img');
+const colors = {
+    yellow: '#FAEA48',
+    darkBlue: '#242F9B' ,
+    lightBlue: '#34B3F1',
+    grey: '#748DA6',
+    lightGrey: '#DAEAF1'
+}
 
 async function getWeather(cityName){
     try{
         let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=fb821944aeb2aeded96b9fa63f6f9094&units=metric',{mode: 'cors'});
         response = await response.json();
-        return {
-            name: response.name,
-            weather: response.weather[0].main,
-            temp: response.main.temp,
-            feelsLike: response.main.feels_like,
-            tempMin: response.main.temp_min,
-            tempMax: response.main.temp_max,
-            humidity: response.main.humidity,
-        };
+        return [
+            response.name,
+            response.weather[0].main,
+            response.main.temp,
+            response.main.feels_like,
+            response.main.temp_min,
+            response.main.temp_max,
+            response.wind.speed,
+            response.main.humidity,
+        ];
     }catch(err){
         console.log(err);
     }
 }
 
-function loadImage(weather){
-    url = "images/" + weather + ".jpg";
-    document.body.style.backgroundImage = 'url(' + url + ')';
+async function getGif(word){
+    let gif = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=J0ehOXOWVBBxadeofKf3mZLxpQbPXOJD&s=' + word, { mode: 'cors'});
+    gif = await gif.json();
+    img.src = gif.data.images.original.url;
+}
+
+function changeBgColor(data){
+    let bgColor = '';
+    if(data.includes('Clear')) bgColor = 'lightBlue';
+    if(data.includes('Cloud')) bgColor = 'yellow';
+    if(data.includes('Rain')) bgColor = 'darkBlue';
+    if(data.includes('Mist')) bgColor = 'grey';
+    if(data.includes('Snow')) bgColor = 'lightGreygrey';
+    document.body.style.backgroundColor = colors[bgColor];
 }
 
 function updateScreen(data){
-    loadImage(data.weather);
+    getGif(data[1]);
+    changeBgColor(data[1]);
+    main.querySelector('h1').textContent = data[0];
+    for (let i = 0; i < output.length; i++) {
+        output[i].querySelector('p').textContent = data[i+1];
+    }
+    main.style.display = 'block';
 
-    output.querySelector('h1').textContent = data.name;
-    let paras = output.querySelectorAll('p');
-    paras[0].textContent = 'Weather : ' + data.weather;
-    paras[1].textContent = 'Temprature : ' + data.temp;
-    paras[2].textContent = 'Feels like : ' + data.feelsLike;
-    paras[3].textContent = 'Minimum Temprature : ' + data.tempMin;
-    paras[4].textContent = 'Maximum Temprature : ' + data.tempMax;
-    paras[5].textContent = 'Humidity : ' + data.humidity + '%';
 }
 
 input.addEventListener('change', function(e){
