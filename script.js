e7cf498a332c4bab5c4c48afc206f2894d95a6a2
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-07-01 06:57:03 -0700

    fetch weather data and display in console

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let place = prompt('Enter location.');
 
+fetch('https://api.openweathermap.org/data/2.5/weather?q=' + place + '&appid=fb821944aeb2aeded96b9fa63f6f9094&units=metric',{mode: 'cors'})
+.then(response => {
+  return response.json();
+}).then(response2 => {
+  console.log('City : ' + response2.name);
+  console.log('Country Code : ' + response2.sys.country);
+  console.log('Temprature : ' + response2.main.temp);
+  console.log('Weather : ' + response2.weather[0].main);
+}).catch(err => {
+  console.log(err);
+});
